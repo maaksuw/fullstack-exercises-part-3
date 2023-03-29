eea9fe664e6624e34f084c0cb8d6dcd8e3cbef69
Vierimaa Pinja K <vwpinja@cubbli22-gold.local>
2023-03-29 19:21:05 +0300

    Tehtävä 3.1, puhelinluettelo backend osa 1.

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+	{
+		"id": 1,
+		"name": "Makkis Makkarahauva",
+		"number": "111-111-111"
+	},
+	{
+		"id": 2,
+		"name": "Pupu Jussikainen",
+		"number": "222-222-222"
+	},
+	{
+		"id": 3,
+		"name": "Misse Missenen",
+		"number": "333-333-333"
+	}
+]
+
+app.get('/api/persons', (req, res) => {
+	res.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
+})
