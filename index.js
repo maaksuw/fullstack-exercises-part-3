a7b5a728aa275d49f0d83d9296f3e5894eb8a5f9
Vierimaa Pinja K <vwpinja@cubbli22-gold.local>
2023-03-29 19:48:39 +0300

    Tehtävä 3.4, puhelinluettelo backend osa 4.

--- index.js
+++ index.js
@@ -38,6 +38,17 @@ app.get('/api/persons/:id', (request, response) => {
 	}
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+	const found = persons.some(person => person.id === id)
+	if (found) {
+		persons = persons.filter(person => person.id !== id)
+  	response.status(204).end()
+	} else {
+		response.status(404).end()
+	}
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
